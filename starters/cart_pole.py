b1b4d3debe62211cbcdb647b7ebb6966f23ac8c5
lets go <84551063+mesophil@users.noreply.github.com>
2023-10-01 20:26:21 -0600

    Tuning cart pole

--- starters/cart_pole.py
+++ starters/cart_pole.py
@@ -4,9 +4,6 @@ from stable_baselines3 import PPO
 from stable_baselines3.ppo import MlpPolicy
 from stable_baselines3.common.evaluation import evaluate_policy
 
-def train(model):
-    model.learn(total_timesteps=100_000)
-
 def eval(model):
     eval_env = gym.make("CartPole-v1", render_mode="rgb_array")
     mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=1000)
@@ -17,8 +14,8 @@ def main():
     train_env = gym.make("CartPole-v1", render_mode="rgb_array")
 
     model = PPO(MlpPolicy, train_env, verbose=0)
+    model.learn(total_timesteps=100_000)
 
-    train(model)
     mean_reward, std_reward = eval(model)
 
     print(f"mean reward: {mean_reward:.3f} +/- {std_reward:.3f}")
